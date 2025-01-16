adb6160b71cd249f16b3a36f08134877850cc834
Maga <uvaysov.2005@mail.ru>
2025-01-17 00:58:03 +0300

    Annotation

--- orders/services.py
+++ orders/services.py
@@ -1,17 +1,19 @@
-import datetime
+from datetime import datetime
 
 from django.db.models import QuerySet
+from django.http import QueryDict
 
 from .models import Order
 
 # Подсчитывает сумму выручки от и до определенных дат
-def calculate_cash_sum(from_date, to_date):
+def calculate_cash_sum(from_date: datetime, to_date: datetime):
     return sum([int(n.total_price(as_int=True)) for n in Order.objects.filter(status__lte=2,
                                                               paid_date__gte=from_date,
                                                               paid_date__lte=to_date)])
 
 # Задает дату оплаты или стирает дату оплаты в зависимости от того какой статус был до этого
-def set_paid_date(status_before, status_after, order):
+def set_paid_date(status_before: int, status_after: int, order: Order):
+    print(type(order))
     if status_before == status_after:
         return
     elif status_before == 3:
@@ -22,7 +24,7 @@ def set_paid_date(status_before, status_after, order):
         order.save()
 
 # Возвращает список объектов, отфильтрованных по переданным в GET запросе значениям
-def get_filtered_orders(data):
+def get_filtered_orders(data: QueryDict):
     status = data.get('status')
     tn = data.get('tn')
     if tn:
