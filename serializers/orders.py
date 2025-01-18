9e5b46cce0e569673905ccac40b012d9d1a128b5
slaverchief <uvaysov.2005@mail.ru>
2025-01-18 18:17:53 +0300

    OrderSerializer depth is 2

--- serializers/orders.py
+++ serializers/orders.py
@@ -2,7 +2,7 @@ from .base import BaseCafeteriaSerializer
 from apps.orders.models import Order
 
 class OrderSerializer(BaseCafeteriaSerializer):
-
     class Meta:
         model = Order
         fields = '__all__'
+        depth = 2
